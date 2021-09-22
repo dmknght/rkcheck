eec06e1b1bee0f008fd030c06c9829b936199908
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-23 06:42:16 +0700

    Add callback

--- src/rkcompiler.nim
+++ src/rkcompiler.nim
@@ -4,38 +4,51 @@
 
 import libyara / nimyara
 import os
-import strutils
+import strformat
+
+type
+  COMPILER_RESULT = object
+    errors: int
+    warnings: int
 
 
-proc compile_rules(src, dst: string) =
+proc report_error(error_level: cint; file_name: cstring; line_number: cint; rule: ptr YR_RULE; message: cstring; user_data: pointer) {.cdecl.} =
+  if rule != nil:
+    echo fmt"{message} at {file_name}:{line_number}"
+
+
+proc compile_rules(dst: string) =
   # Init yara
   var
     compiler: ptr YR_COMPILER
     rules: ptr YR_RULES
+    compiler_result: COMPILER_RESULT
+
   let setting_max_string = DEFAULT_MAX_STRINGS_PER_RULE
+
   if yr_initialize() != ERROR_SUCCESS:
     return
   if yr_compiler_create(addr(compiler)) != ERROR_SUCCESS:
     return
-  discard yr_set_configuration(YR_CONFIG_MAX_STRINGS_PER_RULE, unsafeAddr(setting_max_string))
 
   # Load yara rules
-  if fileExists(src):
-    discard yr_compiler_define_string_variable(compiler, "file_path", "")
-    discard yr_compiler_define_string_variable(compiler, "file_name", "")
-    discard yr_compiler_define_string_variable(compiler, "file_dir", "")
-    discard yr_compiler_define_string_variable(compiler, "file_ext", "")
-    discard yr_compiler_add_file(compiler, open(src), nil, src)
-    discard yr_compiler_get_rules(compiler, addr(rules))
-    discard yr_rules_save(rules, dst)
-  elif dirExists(src):
-    for kind, path in walkDir(src):
-      if kind == pcFile and splitFile(path).ext == ".yara" or splitFile(path).ext == ".yar":
-        discard yr_compiler_add_file(compiler, open(path), nil, path.split("/")[^1])
-        discard yr_compiler_get_rules(compiler, addr(rules))
-    discard yr_rules_save(rules, dst)
-  else:
-    echo "Invalid file or dir ", src
+
+  discard yr_compiler_define_string_variable(compiler, "file_path", "")
+  discard yr_compiler_define_string_variable(compiler, "file_name", "")
+  discard yr_compiler_define_string_variable(compiler, "file_dir", "")
+  discard yr_compiler_define_string_variable(compiler, "file_ext", "")
+
+  discard yr_set_configuration(YR_CONFIG_MAX_STRINGS_PER_RULE, unsafeAddr(setting_max_string))
+  yr_compiler_set_callback(compiler, report_error, unsafeAddr(compiler_result))
+
+  # discard yr_compiler_add_file(compiler, open("rules/rootkit.yar"), "rootkit", "rootkit.yar")
+  discard yr_compiler_add_file(compiler, open("rules/trojan.yar"), "trojan", "trojan.yar")
+  # discard yr_compiler_add_file(compiler, open("rules/coin_miner.yar"), "coin_miner", "coin_miner.yar")
+  # discard yr_compiler_add_file(compiler, open("rules/botnet.yar"), "botnet", "botnet.yar")
+
+  discard yr_compiler_get_rules(compiler, addr(rules))
+  discard yr_rules_save(rules, dst)
+
 
   # finityara
   if compiler != nil:
@@ -48,7 +61,4 @@ proc compile_rules(src, dst: string) =
 if not dirExists("database"):
   createDir("database")
 
-compile_rules("rules/rootkit.yar", "database/rootkit.ydb")
-compile_rules("rules/trojan.yar", "database/trojan.ydb")
-compile_rules("rules/coin_miner.yar", "database/coin_miner.ydb")
-compile_rules("rules/botnet.yar", "database/botnet.ydb")
+compile_rules("database/signatures.ydb")
