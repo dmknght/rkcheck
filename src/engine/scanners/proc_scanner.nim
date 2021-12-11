745b361e1369cff3c76551d8d0e9243c43380839
Nong Hoang Tu <dmknght@parrotsec.org>
2021-12-11 18:47:16 +0700

    Improve binary print and fix missing path for scan procs

--- src/engine/scanners/proc_scanner.nim
+++ src/engine/scanners/proc_scanner.nim
@@ -9,7 +9,8 @@ proc cb_yr_process_scan(context: ptr YR_SCAN_CONTEXT; message: cint; message_dat
   if message == CALLBACK_MSG_RULE_MATCHING:
     let
       data = cast[ptr ProcInfo](user_data)
-    echo cast[ptr YR_RULE](message_data).ns.name, ":", cast[ptr YR_RULE](message_data).identifier, " ", data.binary_path, " (pid: ", data.pid, ")"
+      rule = cast[ptr YR_RULE](message_data)
+    echo rule.ns.name, ":", rule.identifier, " ", data.binary_path, " (pid: ", data.pid, ")"
     return CALLBACK_ABORT
   else:
     # cast[ProcScanContext](user_data).virus_name = ""
@@ -18,6 +19,11 @@ proc cb_yr_process_scan(context: ptr YR_SCAN_CONTEXT; message: cint; message_dat
 
 proc rscanner_scan_proc(context: var ProcScanContext) =
   context.scan_object.cmdline = readFile(context.scan_object.pid_path & "/cmdline")
+  try:
+    context.scan_object.binary_path = expandSymlink(context.scan_object.pid_path & "/exe")
+  except:
+    # Fix crash when pid = 1 -> exe permission denied
+    context.scan_object.binary_path = context.scan_object.cmdline
   # TODO handle parent pid, child pid, ... to do ignore scan
   # TODO sometime the actual malicious part is cmdline (python3 -c <reverse shell> for example. We scan it as well)
   discard yr_rules_scan_proc(
@@ -35,7 +41,7 @@ proc rscanner_new_proc_scan*(engine: CoreEngine, pid: int) =
   ScanContext.ScanEngine = engine
   ScanContext.scan_object.pid = pid
   ScanContext.scan_object.pid_path = "/proc/" & intToStr(pid)
-  ScanContext.scan_object.binary_path = expandSymlink(ScanContext.scan_object.pid_path & "/exe")
+
   rscanner_scan_proc(ScanContext)
 
 
@@ -52,4 +58,3 @@ proc rscanner_new_procs_scan*(engine: CoreEngine) =
       except ValueError:
         # This is not a process from procfs
         discard
-
