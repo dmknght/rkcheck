15e3a3846035d2b34c2d5abc242c29df73858aeb
Nong Hoang Tu <dmknght@parrotsec.org>
2022-10-13 05:09:30 +0700

    Add callback to call progress bar

--- newsrc/engine/file_scan.nim
+++ newsrc/engine/file_scan.nim
@@ -5,16 +5,6 @@ import engine_utils
 import .. / cli / progress_bar
 
 
-proc fscanner_cb_clam_virus_found*(fd: cint, virname: cstring, context: pointer) {.cdecl.} =
-  #[
-    Print virus found message with file path
-  ]#
-  let
-    ctx = cast[ptr FileScanner](context)
-
-  file_scanner_on_malware_found(virname, ctx.scan_virname, ctx.scan_object, ctx.result_infected)
-
-
 proc fscanner_cb_yara_scan_result*(context: ptr YR_SCAN_CONTEXT; message: cint; message_data: pointer; user_data: pointer): cint {.cdecl.} =
   #[
     Handle scan result from Yara engine
@@ -32,7 +22,7 @@ proc fscanner_cb_yara_scan_result*(context: ptr YR_SCAN_CONTEXT; message: cint; 
     return file_scanner_on_clean(ctx.scan_result, ctx.scan_virname)
 
 
-proc fscanner_cb_clam_scan_file*(fd: cint, `type`: cstring, context: pointer): cl_error_t {.cdecl.} =
+proc fscanner_cb_scan_file*(fd: cint, `type`: cstring, context: pointer): cl_error_t {.cdecl.} =
   #[
     The actual function to scan files. This function will call yara to scan file first.
     If result is CL_CLEAN, Clam will scan with its signatures
@@ -41,8 +31,28 @@ proc fscanner_cb_clam_scan_file*(fd: cint, `type`: cstring, context: pointer): c
   let
     ctx = cast[ptr FileScanner](context)
 
-  progress_bar_scan_file(ctx.scan_object)
   discard yr_rules_scan_fd(ctx.yr_scanner.engine, fd, SCAN_FLAGS_FAST_MODE, fscanner_cb_yara_scan_result, context, YR_SCAN_TIMEOUT)
-  progress_bar_flush()
+  # TODO use libclam debug to show errors if scanner returns != 0
 
   return ctx.scan_result
+
+
+proc fscanner_cb_pre_cache*(fd: cint, `type`: cstring, context: pointer): cl_error_t {.cdecl.} =
+  let
+    ctx = cast[ptr FileScanner](context)
+
+  progress_bar_scan_file(ctx.scan_object)
+
+
+proc fscanner_cb_post_scan*(fd: cint, `type`: cstring, context: pointer): cl_error_t {.cdecl.} =
+  progress_bar_flush()
+
+
+proc fscanner_cb_virus_found*(fd: cint, virname: cstring, context: pointer) {.cdecl.} =
+  #[
+    Print virus found message with file path
+  ]#
+  let
+    ctx = cast[ptr FileScanner](context)
+
+  file_scanner_on_malware_found(virname, ctx.scan_virname, ctx.scan_object, ctx.result_infected)
