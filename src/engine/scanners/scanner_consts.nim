3edc97f3ee651f9c830d4de28d6dfa109237c18b
Nong Hoang Tu <dmknght@parrotsec.org>
2022-11-02 03:14:38 +0700

    Use the new rewrite source

--- src/engine/scanners/scanner_consts.nim
+++
@@ -1,5 +0,0 @@
-const
-  sys_dir_proc* = "/proc/"
-  sys_dir_autostart* = "/etc/xdg/autostart/"
-  home_dir_autostart* = ".config/autostart/"
-# TODO systemd units as well
---
+++ src/testfile.nim
@@ -0,0 +1,10 @@
+import scanners / scanners
+
+
+
+# create_scan_task(list_dirs = @["/home/dmknght/Desktop/MalwareLab/LinuxMalwareDetected/"])
+# create_scan_task(list_files = @["/home/dmknght/Desktop/MalwareLab/Linux-Malware-Samples/329255e33f43e6e9ae5d5efd6f5c5745c35a30d42fb5099beb51a6e40fe9bd76"])
+# create_scan_task(list_files = @["/home/dmknght/Desktop/MalwareLab/LinuxMalwareDetected_save/0b22cdc1b1b1f944e4ca8fced2e234d14aeeef830970e8ae7491cbdcb3e11460_detected_detected"])
+# create_scan_task(list_dirs = @["/home/dmknght/Desktop/MalwareLab/Linux-Malware-Samples"])
+# create_scan_task(list_dirs = @["/usr/bin/"])
+create_scan_task()
