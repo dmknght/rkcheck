e7b1ca20214660c4ac5b5267c183722e18d9204c
Nong Hoang Tu <dmknght@parrotsec.org>
2022-11-09 07:46:15 +0700

    Create mechanic to handle keyboard interrupt

--- src/scanners/scanners.nim
+++ src/scanners/scanners.nim
@@ -2,6 +2,14 @@ import os
 import .. / engine / [libyara, libclamav, engine_cores, scan_file, scan_proc]
 
 
+type
+  KeyboardInterrupt = object of CatchableError
+
+
+proc handle_keyboard_interrupt() {.noconv.} =
+  raise newException(KeyboardInterrupt, "Keyboard Interrupt")
+
+
 proc create_task_file_scan(yara_engine: YrEngine, options: ScanOptions, result_count, result_infect: var uint) =
   var
     file_scanner: FileScanner
@@ -30,20 +38,23 @@ proc create_task_file_scan(yara_engine: YrEngine, options: ScanOptions, result_c
   cl_engine_set_clcb_virus_found(file_scanner.engine, fscanner_cb_virus_found)
   cl_set_clcb_msg(fscanner_cb_msg_dummy)
 
-  if len(options.list_dirs) != 0:
-    for dir_path in options.list_dirs:
-      for path in walkDirRec(dir_path):
+  try:
+    if len(options.list_dirs) != 0:
+      for dir_path in options.list_dirs:
+        for path in walkDirRec(dir_path):
+          file_scanner.scan_object = path
+          discard cl_scanfile_callback(cstring(path), addr(virname), addr(scanned), file_scanner.engine, addr(file_scanner.options), addr(file_scanner))
+
+    if len(options.list_files) != 0:
+      for path in options.list_files:
         file_scanner.scan_object = path
         discard cl_scanfile_callback(cstring(path), addr(virname), addr(scanned), file_scanner.engine, addr(file_scanner.options), addr(file_scanner))
-
-  if len(options.list_files) != 0:
-    for path in options.list_files:
-      file_scanner.scan_object = path
-      discard cl_scanfile_callback(cstring(path), addr(virname), addr(scanned), file_scanner.engine, addr(file_scanner.options), addr(file_scanner))
-
-  result_count = file_scanner.result_scanned
-  result_infect = file_scanner.result_infected
-  finit_clamav(file_scanner)
+  except KeyboardInterrupt:
+    return
+  finally:
+    result_count = file_scanner.result_scanned
+    result_infect = file_scanner.result_infected
+    finit_clamav(file_scanner)
 
 
 proc create_task_proc_scan(yara_engine: YrEngine, options: ScanOptions, result_count, result_infected: var uint) =
@@ -54,14 +65,17 @@ proc create_task_proc_scan(yara_engine: YrEngine, options: ScanOptions, result_c
   proc_scan_engine.sumary_infected = 0
   proc_scan_engine.engine = yara_engine.engine
 
-  if options.scan_all_procs:
-    pscanner_scan_system_procs(proc_scan_engine)
-  else:
-    pscanner_scan_procs(proc_scan_engine, options.list_procs)
-
-  result_count = proc_scan_engine.sumary_scanned
-  result_infected = proc_scan_engine.sumary_infected
-  finit_yara(proc_scan_engine)
+  try:
+    if options.scan_all_procs:
+      pscanner_scan_system_procs(proc_scan_engine)
+    else:
+      pscanner_scan_procs(proc_scan_engine, options.list_procs)
+  except KeyboardInterrupt:
+    return
+  finally:
+    result_count = proc_scan_engine.sumary_scanned
+    result_infected = proc_scan_engine.sumary_infected
+    finit_yara(proc_scan_engine)
 
 
 proc create_scan_task*(options: ScanOptions, f_count, f_infect, p_count, p_infect: var uint) =
@@ -69,6 +83,7 @@ proc create_scan_task*(options: ScanOptions, f_count, f_infect, p_count, p_infec
     yara_engine: YrEngine
 
   yara_engine.database = options.db_path_yara
+  setControlCHook(handle_keyboard_interrupt)
 
   if yara_engine.init_yara() != ERROR_SUCCESS:
     raise newException(ValueError, "Failed to init Yara Engine")
