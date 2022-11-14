0fb7439ab80f4d59d47db0ea918bf0e1d49ebc2e
Nong Hoang Tu <dmknght@parrotsec.org>
2022-11-14 07:23:19 +0700

    Code clean

--- src/rkscanrootkit.nim
+++ src/rkscanrootkit.nim
@@ -11,7 +11,6 @@ elif not dirExists("/sys/"):
   raise newException(OSError, "Sysfs is not mounted")
 else:
   const
-    # FIXME: LibClamAV debug: cl_scandesc_callback: File too small (0 bytes), ignoring
     kernel_modules = "/sys/kernel/tracing/available_filter_functions"
   var
     options: ScanOptions
@@ -19,7 +18,8 @@ else:
 
   options.cliopts_create_default(true)
   options.list_files = @[kernel_modules]
-  # TODO match all rules
+  # TODO scan /etc/ld.so.preload
+  # https://www.sandflysecurity.com/blog/log4j-kinsing-linux-malware-in-the-wild/
   create_scan_rootkit_task(options, f_infect)
-  # TODO show sumary
+  # TODO show sumary that has kernel modules and ld-preload modules
   # print_sumary(f_count, f_infect, p_count, p_infect)
