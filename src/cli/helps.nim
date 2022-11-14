a0da271b26d59de46f77ecc1b9534b491b69334d
Nong Hoang Tu <dmknght@parrotsec.org>
2022-11-14 07:44:30 +0700

    Shorter help description

--- src/cli/helps.nim
+++ src/cli/helps.nim
@@ -7,7 +7,7 @@ proc show_help_banner*(): bool =
   echo " --path-clamdb  <file or dir>  Set custom ClamAV's signatures"
   echo " --path-yaradb  <file>         Set custom Yara's rules"
   echo "\nScan options:"
-  echo "  --check-hidden-proc          Do procfs walkDir to check if processes are hidden"
+  echo "  --check-hidden-proc          Check if processes are hidden"
   echo "  --all-processes              Scan all running proccesses"
   echo "  --list-dirs   <dir1 dir2>    Scan directories"
   echo "  --list-files  <file1 file2>  Scan files"
