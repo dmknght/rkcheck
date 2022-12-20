7c844ee712d9e714372f72929789fe18d765d6a4
Nong Hoang Tu <dmknght@parrotsec.org>
2022-12-21 00:08:20 +0700

    Do not compile rootkit module

--- src/compiler/yr_db_compiler.nim
+++ src/compiler/yr_db_compiler.nim
@@ -40,30 +40,30 @@ proc compile_default_rules(dst: string) =
   discard yr_finalize()
 
 
-proc compile_rootkit_modules_rules(dst: string) =
-  var
-    compiler: ptr YR_COMPILER
-    rules: ptr YR_RULES
-    compiler_result: COMPILER_RESULT
-    setting_max_string = DEFAULT_MAX_STRINGS_PER_RULE
-  if yr_initialize() != ERROR_SUCCESS:
-    return
-  if yr_compiler_create(addr(compiler)) != ERROR_SUCCESS:
-    return
+# proc compile_rootkit_modules_rules(dst: string) =
+#   var
+#     compiler: ptr YR_COMPILER
+#     rules: ptr YR_RULES
+#     compiler_result: COMPILER_RESULT
+#     setting_max_string = DEFAULT_MAX_STRINGS_PER_RULE
+#   if yr_initialize() != ERROR_SUCCESS:
+#     return
+#   if yr_compiler_create(addr(compiler)) != ERROR_SUCCESS:
+#     return
 
-  discard yr_set_configuration(YR_CONFIG_MAX_STRINGS_PER_RULE, addr(setting_max_string))
-  yr_compiler_set_callback(compiler, yr_rules_report_errors, addr(compiler_result))
+#   discard yr_set_configuration(YR_CONFIG_MAX_STRINGS_PER_RULE, addr(setting_max_string))
+#   yr_compiler_set_callback(compiler, yr_rules_report_errors, addr(compiler_result))
 
-  discard yr_compiler_add_file(compiler, open("rules/rootkit_modules.yar"), "Rootkit", "rootkit_modules.yar")
+#   discard yr_compiler_add_file(compiler, open("rules/rootkit_modules.yar"), "Rootkit", "rootkit_modules.yar")
 
-  discard yr_compiler_get_rules(compiler, addr(rules))
-  discard yr_rules_save(rules, dst)
-  # finityara
-  if compiler != nil:
-    yr_compiler_destroy(compiler)
-  if rules != nil:
-    discard yr_rules_destroy(rules)
-  discard yr_finalize()
+#   discard yr_compiler_get_rules(compiler, addr(rules))
+#   discard yr_rules_save(rules, dst)
+#   # finityara
+#   if compiler != nil:
+#     yr_compiler_destroy(compiler)
+#   if rules != nil:
+#     discard yr_rules_destroy(rules)
+#   discard yr_finalize()
 
 compile_default_rules("build/database/signatures.ydb")
-compile_rootkit_modules_rules("build/database/rootkits.ydb")
+# compile_rootkit_modules_rules("build/database/rootkits.ydb")
