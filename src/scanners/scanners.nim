ee09fce9de589bbec0176f36fd4d737c08e7befe
Nong Hoang Tu <dmknght@parrotsec.org>
2022-12-24 08:03:37 +0700

    Improve code structure of th scanners

--- src/scanners/scanners.nim
+++ src/scanners/scanners.nim
@@ -11,20 +11,15 @@ proc handle_keyboard_interrupt() {.noconv.} =
   raise newException(KeyboardInterrupt, "Keyboard Interrupt")
 
 
-proc scanners_create_task_file_scan(yara_engine: YrEngine, options: ScanOptions, result_count, result_infect: var uint) =
-  var
-    file_scanner: FileScanner
-    scanned: culong
-    virname: cstring
+proc scanners_set_clamav_values(scanner: var FileScanner, yara_engine: YrEngine, options: ScanOptions) =
+  scanner.yr_scanner = yara_engine
+  scanner.result_infected = 0
+  scanner.result_scanned = 0
+  scanner.debug_mode = options.is_clam_debug
+  scanner.database = options.db_path_clamav
+  scanner.use_clam_sigs = options.use_clam_db
 
-  file_scanner.yr_scanner = yara_engine
-  file_scanner.result_infected = 0
-  file_scanner.result_scanned = 0
-  file_scanner.debug_mode = options.is_clam_debug
-  file_scanner.database = options.db_path_clamav
-  file_scanner.use_clam_sigs = options.use_clam_db
-
-  if file_scanner.init_clamav() != ERROR_SUCCESS:
+  if scanner.init_clamav() != ERROR_SUCCESS:
     raise newException(ValueError, "Failed to init ClamAV Engine")
 
   #[
@@ -34,9 +29,18 @@ proc scanners_create_task_file_scan(yara_engine: YrEngine, options: ScanOptions,
     3. post_scan: after file scan complete
     4. virus_found: only when a virus is found
   ]#
-  cl_engine_set_clcb_post_scan(file_scanner.engine, fscanner_cb_scan_file)
-  cl_engine_set_clcb_virus_found(file_scanner.engine, fscanner_cb_virus_found)
+  cl_engine_set_clcb_post_scan(scanner.engine, fscanner_cb_scan_file)
+  cl_engine_set_clcb_virus_found(scanner.engine, fscanner_cb_virus_found)
   cl_set_clcb_msg(fscanner_cb_msg_dummy)
+
+
+proc scanners_cl_scan_files(yara_engine: YrEngine, options: ScanOptions, result_count, result_infect: var uint) =
+  var
+    file_scanner: FileScanner
+    scanned: culong
+    virname: cstring
+
+  scanners_set_clamav_values(file_scanner, yara_engine, options)
 
   try:
     if len(options.list_dirs) != 0:
@@ -57,7 +61,7 @@ proc scanners_create_task_file_scan(yara_engine: YrEngine, options: ScanOptions,
     finit_clamav(file_scanner)
 
 
-proc scanners_create_task_file_scan_yr(yara_engine: var YrFileScanner, options: ScanOptions, result_count, result_infect: var uint) =
+proc scanners_yr_scan_files(yara_engine: var YrFileScanner, options: ScanOptions, result_count, result_infect: var uint) =
   try:
     if len(options.list_dirs) != 0:
       for dir_path in options.list_dirs:
@@ -77,18 +81,22 @@ proc scanners_create_task_file_scan_yr(yara_engine: var YrFileScanner, options: 
     finit_yara(yara_engine)
 
 
-proc scanners_create_task_proc_scan(yara_engine: YrEngine, options: ScanOptions, result_count, result_infected: var uint) =
+proc scanners_set_proc_scan_values(scanner: var ProcScanner, options: ScanOptions, engine: ptr YR_RULES) =
+  if options.match_all:
+    scanner.match_all_rules = true
+  else:
+    scanner.match_all_rules = false
+
+  scanner.sumary_scanned = 0
+  scanner.sumary_infected = 0
+  scanner.engine = engine
+
+
+proc scanners_yr_scan_procs(yara_engine: YrEngine, options: ScanOptions, result_count, result_infected: var uint) =
   var
     proc_scanner: ProcScanner
 
-  if options.match_all:
-    proc_scanner.match_all_rules = true
-  else:
-    proc_scanner.match_all_rules = false
-
-  proc_scanner.sumary_scanned = 0
-  proc_scanner.sumary_infected = 0
-  proc_scanner.engine = yara_engine.engine
+  scanners_set_proc_scan_values(proc_scanner, options, yara_engine.engine)
 
   try:
     if options.scan_all_procs:
@@ -114,10 +122,10 @@ proc scanners_create_scan_task*(options: ScanOptions, f_count, f_infect, p_count
     raise newException(ValueError, "Failed to init Yara Engine")
 
   if len(options.list_files) != 0 or len(options.list_dirs) != 0:
-    scanners_create_task_file_scan(yara_engine, options, f_count, f_infect)
+    scanners_cl_scan_files(yara_engine, options, f_count, f_infect)
 
   if len(options.list_procs) != 0 or options.scan_all_procs:
-    scanners_create_task_proc_scan(yara_engine, options, p_count, p_infect)
+    scanners_yr_scan_procs(yara_engine, options, p_count, p_infect)
 
 
 proc scanners_create_scan_preload*(options: var ScanOptions, f_count, f_infect, p_count, p_infect: var uint) =
@@ -140,7 +148,7 @@ proc scanners_create_scan_preload*(options: var ScanOptions, f_count, f_infect, 
   options.list_files = deduplicate(options.list_files)
 
   if len(options.list_files) != 0 or len(options.list_dirs) != 0:
-    scanners_create_task_file_scan_yr(yara_engine, options, f_count, f_infect)
+    scanners_yr_scan_files(yara_engine, options, f_count, f_infect)
 
   if len(options.list_procs) != 0 or options.scan_all_procs:
-    scanners_create_task_proc_scan(yara_engine, options, p_count, p_infect)
+    scanners_yr_scan_procs(yara_engine, options, p_count, p_infect)
