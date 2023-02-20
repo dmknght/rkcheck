c3dc55c5fd649cafd04c0e9ea31ccedb4e67ecd9
Nong Hoang Tu <dmknght@parrotsec.org>
2023-02-20 11:06:10 +0700

    Shorter description in help

--- src/cli/helps.nim
+++ src/cli/helps.nim
@@ -2,7 +2,7 @@
 
 proc show_help_banner*(): bool =
   echo "\nEngine options:"
-  echo " --use-clamdb                  Use default ClamAV's signatures at /var/lib/clamav/"
+  echo " --use-clamdb                  Use ClamAV's default sigs (/var/lib/clamav/)"
   echo " --clam-debug                  Enable libclam debug mode"
   echo " --path-clamdb  <file or dir>  Set custom ClamAV's signatures"
   echo " --path-yaradb  <file>         Set custom Yara's rules"
