827360e41366897e520f32e3921fc7797f39d797
Nong Hoang Tu <dmknght@parrotsec.org>
2023-04-28 04:58:50 +0700

    Fix spacing of options

--- src/cli/helps.nim
+++ src/cli/helps.nim
@@ -7,8 +7,8 @@ proc show_help_banner*(): bool =
   echo " --path-clamdb  <file or dir>  Set custom ClamAV's signatures"
   echo " --path-yaradb  <file>         Set custom Yara's rules"
   echo "\nScan options:"
-  echo "  --scan-files  <file1 file2>  Scan files and dirs"
-  echo "  --scan-procs  <pid1 pid2>    Scan processes. Skip if --all-procs is used"
-  echo "  --scan-mem                   Scan all running proccesses"
-  echo "  --match-all                  Match all rules (process scan only)"
+  echo " --scan-files  <file1 file2>  Scan files and dirs"
+  echo " --scan-procs  <pid1 pid2>    Scan processes. Skip if --all-procs is used"
+  echo " --scan-mem                   Scan all running proccesses"
+  echo " --match-all                  Match all rules (process scan only)"
   return false
