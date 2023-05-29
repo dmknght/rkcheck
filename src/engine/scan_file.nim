291f50dada55f90e14396c0b0492272dd3f1c45e
Nong Hoang Tu <dmknght@parrotsec.org>
2023-05-29 20:31:55 +0700

    Use pre_scan callback when scan with yara, improve scan speed

--- src/engine/scan_file.nim
+++ src/engine/scan_file.nim
@@ -35,25 +35,42 @@ proc fscanner_cb_virus_found*(fd: cint, virname: cstring, context: pointer) {.cd
   file_scanner_on_malware_found(virname, ctx.yr_scanner.scan_virname, ctx.yr_scanner.scan_object, ctx.yr_scanner.file_infected)
 
 
-proc fscanner_cb_scan_file*(fd: cint, scan_result: cint, virname: cstring, context: pointer): cl_error_t {.cdecl.} =
-  # Inner file name can be called via cli_ctx in others.h of ClamAV but it's not callable
+# proc fscanner_cb_post_scan_file*(fd: cint, scan_result: cint, virname: cstring, context: pointer): cl_error_t {.cdecl.} =
+#   #[
+#     Post-scan callback
+#   ]#
+#   # Inner file name can be called via cli_ctx in others.h of ClamAV but it's not callable
+#   let
+#     ctx = cast[ptr FileScanner](context)
+
+#   progress_bar_scan_file(ctx.yr_scanner.scan_object)
+#   ctx.yr_scanner.file_scanned += 1
+
+#   if scan_result == CL_VIRUS:
+#     ctx.yr_scanner.scan_virname = virname
+#     #[
+#       This is the post-scan step (after scan file)
+#       So if file is marked as virus, we should return CLEAN
+#       So the callback virus found wont be called multiple times
+#     ]#
+#     return CL_CLEAN
+#   else:
+#     discard yr_rules_scan_fd(ctx.yr_scanner.engine, fd, SCAN_FLAGS_FAST_MODE, fscanner_cb_yara_scan_result, context, YR_SCAN_TIMEOUT)
+#     return ctx.yr_scanner.scan_result
+
+
+proc fscanner_cb_pre_scan_file*(fd: cint, cl_type: cstring, context: pointer): cl_error_t {.cdecl.} =
+  #[
+    Pre-scan callback
+  ]#
   let
     ctx = cast[ptr FileScanner](context)
 
   progress_bar_scan_file(ctx.yr_scanner.scan_object)
   ctx.yr_scanner.file_scanned += 1
 
-  if scan_result == CL_VIRUS:
-    ctx.yr_scanner.scan_virname = virname
-    #[
-      This is the post-scan step (after scan file)
-      So if file is marked as virus, we should return CLEAN
-      So the callback virus found wont be called multiple times
-    ]#
-    return CL_CLEAN
-  else:
-    discard yr_rules_scan_fd(ctx.yr_scanner.engine, fd, SCAN_FLAGS_FAST_MODE, fscanner_cb_yara_scan_result, context, YR_SCAN_TIMEOUT)
-    return ctx.yr_scanner.scan_result
+  discard yr_rules_scan_fd(ctx.yr_scanner.engine, fd, SCAN_FLAGS_FAST_MODE, fscanner_cb_yara_scan_result, context, YR_SCAN_TIMEOUT)
+  return ctx.yr_scanner.scan_result
 
 
 proc fscanner_cb_inc_count*(fd: cint, scan_result: cint, virname: cstring, context: pointer): cl_error_t {.cdecl.} =
