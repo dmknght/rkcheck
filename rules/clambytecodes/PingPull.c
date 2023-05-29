76fa71a514d20a69d8166cc58e64cc33659501ff
Nong Hoang Tu <dmknght@parrotsec.org>
2023-05-29 15:40:01 +0700

    Add clamav bytecode to test

---
+++ rules/clambytecodes/PingPull.c
@@ -0,0 +1,23 @@
+VIRUSNAME_PREFIX("Trojan.PingPong")
+VIRUSNAMES("Generic")
+TARGET(6) // ELF file
+
+SIGNATURES_DECL_BEGIN
+DECLARE_SIGNATURE(sdb_to_inittab)
+SIGNATURES_DECL_END
+
+SIGNATURES_DEF_BEGIN
+DEFINE_SIGNATURE(sdb_to_inittab, "6563686f2022747362643a323334353a7265737061776e3a2573202d6622203e3e202f6574632f696e697474616200696e6974")
+SIGNATURES_END
+
+
+bool logical_trigger(void)
+{
+  return matches(Signatures.sdb_to_inittab);
+}
+
+int entrypoint(void)
+{
+  foundVirus("Generic");
+  return 0;
+}
---
+++ rules/clambytecodes/README.md
@@ -0,0 +1,4 @@
+# Try ClamAV bytecodes.
+To get Clam's Bytecode compiler, just clone the repo `https://github.com/Cisco-Talos/clamav-bytecode-compiler/`
+then build `docker build .`
+Run it with mounted volumn `docker run -v /home/dmknght/ParrotProjects/rkcheck/rules/:/tmp/signatures -it 7a692a862786 /bin/bash` (use `docker images ls`). There are 2 images. The heavier one is not the image to run
