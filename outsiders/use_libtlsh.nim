1577f6c9c8e3fc13652dbcf0d42c88d3ee9d505a
Nong Hoang Tu <dmknght@parrotsec.org>
2023-07-21 06:06:22 +0700

    Tlsh binding worked finally

--- outsiders/use_libtlsh.nim
+++ outsiders/use_libtlsh.nim
@@ -9,53 +9,48 @@
 type
   Tlsh {.importcpp: "Tlsh".} = object
 
-# A "hack" to create "new" binding
-# https://nim-lang.org/docs/manual.html#implementation-specific-pragmas-importcpp-pragma
-proc tlsh_new*[T](x: T): ptr T {.importcpp: "(new '*0#@)", nodecl.}
-proc TlshObj*(): Tlsh {.importcpp: "Tlsh(@)".}
-proc tlsh_free*(tlsh: ptr Tlsh) {.importcpp: "#.~Tlsh()".}
-
 # Allow the user to add data in multiple iterations
 # void update(const unsigned char* data, unsigned int len);
-proc update*(tlsh: ptr Tlsh; data: cstring; len: cuint): cint {.importcpp, impTlsh.}
+proc update*(tlsh: Tlsh; data: cstring; len: cuint): cint {.importcpp, impTlsh.}
 
 # To signal the class there is no more data to be added
 # void final(const unsigned char* data = NULL, unsigned int len = 0);
-proc final*(tlsh: ptr Tlsh; data: cstring = nil; len: cuint = 0): void {.importcpp, impTlsh.}
+proc final*(tlsh: Tlsh; data: cstring = nil; len: cuint = 0): void {.importcpp, impTlsh.}
 
 # To get the hex-encoded hash code
 # const char* getHash() const ;
-proc getHash*(tlsh: ptr Tlsh): cstring {.importcpp, impTlsh.}
+proc getHash*(tlsh: Tlsh): cstring {.importcpp, impTlsh.}
 
 # To get the hex-encoded hash code without allocating buffer in TlshImpl - bufSize should be TLSH_STRING_BUFFER_LEN */
 # const char* getHash(char *buffer, unsigned int bufSize) const;
-proc getHash*(tlsh: ptr Tlsh, buffer: cstring, size: uint): cstring {.importcpp, impTlsh.}
+# NOTICE: look like this method is used when the fromTlshStr is called. And the buffer is the variable that contains hash from text
+proc getHash*(tlsh: Tlsh, buffer: cstring, size: uint): cstring {.importcpp, impTlsh.}
 
 # To bring to object back to the initial state */
 # void reset();
-proc reset*(tlsh: ptr Tlsh): void {.importcpp, impTlsh.}
+proc reset*(tlsh: Tlsh): void {.importcpp, impTlsh.}
 
 # Calculate difference
 # int totalDiff(const Tlsh *, bool len_diff=true) const;
-proc totalDiff*(tlsh: ptr Tlsh, compare_hash: ptr Tlsh, len_diff: bool = true): cint {.importcpp, impTlsh.}
+proc totalDiff*(tlsh: Tlsh, compare_hash: ptr Tlsh, len_diff: bool = true): cint {.importcpp, impTlsh.}
 
 # Validate TrendLSH string and reset the hash according to it */
 # int fromTlshStr(const char* str);
-proc fromTlshStr*(tlsh: ptr Tlsh, data: cstring): cint {.importcpp, impTlsh.}
+proc fromTlshStr*(tlsh: Tlsh, data: cstring): cint {.importcpp, impTlsh.}
 
 # Return the version information used to build this library
 # static const char *version();
-proc version*(tlsh: ptr Tlsh): cstring {.importcpp, impTlsh.}
+proc version*(tlsh: Tlsh): cstring {.importcpp, impTlsh.}
 
-var
-  vtlsh = tlsh_new TlshObj()
+# TODO if possible, create a library from this, handling calculating hashes from file pointer / file descriptor
+when isMainModule:
+  var
+    t1, t2: Tlsh
+    str1: cstring = "Test string hello world"
+    str2: cstring = "This string is hello world"
 
-var
-  myData: cstring = "aaaa"
-
-echo vtlsh.version()
-# discard vtlsh.update(myData, cuint(myData.len))
-# vtlsh.final()
-# echo vtlsh.getHash()
-echo vtlsh.getHash(myData, cuint(myData.len))
-vtlsh.tlsh_free()
+  t1.final(str1, 512)
+  t2.final(str2, 512)
+  echo t1.getHash()
+  echo t2.getHash()
+  echo totalDiff(t1, t2.addr)
