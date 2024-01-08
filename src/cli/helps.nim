0ee8aaa09ea10b166120b7947edf1c99f5efff1e
Nong Hoang Tu <dmknght@parrotsec.org>
2024-01-08 15:42:57 +0700

    Update help menu

--- src/cli/helps.nim
+++ src/cli/helps.nim
@@ -5,7 +5,7 @@ proc show_help_banner*(): bool =
   echo " --use-clamdb                  Use ClamAV's default sigs (/var/lib/clamav/)"
   echo " --clam-debug                  Enable libclam debug mode"
   echo " --path-clamdb  <file or dir>  Set custom ClamAV's signatures"
-  echo " --path-yaradb  <file>         Set custom Yara's rules"
+  echo " --path-yaradb  <file or dir>  Set custom Yara's rules"
   echo "\nScan options:"
   echo " --scan-files  <file1 file2>  Scan files and dirs"
   echo " --scan-procs  <pid1 pid2>    Scan processes. Skip if --all-procs is used"
