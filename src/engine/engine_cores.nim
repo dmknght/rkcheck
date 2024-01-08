ee5fcaf53fc877dec093f58a900d137969349397
Nong Hoang Tu <dmknght@parrotsec.org>
2024-01-09 05:49:53 +0700

    Disable PE and ELF module again

--- src/engine/engine_cores.nim
+++ src/engine/engine_cores.nim
@@ -76,14 +76,16 @@ proc init_clamav*(clam_engine: var ClEngine, loaded_sig_count: var uint, path_cl
   clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_XMLDOCS)
   clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_MAIL)
   clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_HTML)
-  clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_ELF)
-  clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_PE)
+  # clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_ELF)
+  # clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_PE)
 
   clam_engine.options.general = bitor(clam_engine.options.general, CL_SCAN_GENERAL_HEURISTICS)
 
   if use_clam:
     # Enable cache
     clam_engine.options.general = bitor(clam_engine.options.general, ENGINE_OPTIONS_DISABLE_CACHE)
+    clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_ELF)
+    clam_engine.options.parse = bitor(clam_engine.options.parse, CL_SCAN_PARSE_PE)
     # Maybe enable macho?
     # clam_engine.options.heuristic = bitor(clam_engine.options.heuristic, CL_SCAN_HEURISTIC_BROKEN)
     # clam_engine.options.heuristic = bitor(clam_engine.options.heuristic, CL_SCAN_HEURISTIC_ENCRYPTED_ARCHIVE)
@@ -92,6 +94,8 @@ proc init_clamav*(clam_engine: var ClEngine, loaded_sig_count: var uint, path_cl
   else:
     # Disable cache
     clam_engine.options.general = bitand(clam_engine.options.general, ENGINE_OPTIONS_DISABLE_CACHE)
+    clam_engine.options.parse = bitand(clam_engine.options.parse, CL_SCAN_PARSE_ELF)
+    clam_engine.options.parse = bitand(clam_engine.options.parse, CL_SCAN_PARSE_PE)
 
   discard clam_engine.engine.cl_engine_set_num(CL_ENGINE_MAX_FILESIZE, 75 * 1024 * 1024) # Max scan size 60mb
 
