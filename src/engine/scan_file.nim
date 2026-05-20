e9072425d2c8f8cf54a494dcabd5b39eb868cf80
Nong Hoang Tu <dmknght@parrotsec.org>
2026-05-20 08:26:16 +0700

    Set max size to scan file is 5mb instead of 1mb. This doesn't improve performance that much but it's a try.

--- src/engine/scan_file.nim
+++ src/engine/scan_file.nim
@@ -109,9 +109,8 @@ proc fscanner_cb_file_inspection*(fd: cint, file_type: cstring, ancestors: ptr c
       else:
         ctx.virt_scan_object = ctx.scan_object & "//" & inner_file_name
 
-  if file_size <= 1048576: # 1024 * 1024
+  if file_size <= 5242880: # 1024 * 1024 * 5. Max scan mem 5mb instead
     # If object is small, use scan mem instead. This avoids remapping again. However, this could also break the metadata mapping
-    # discard yr_rules_scan_mem(ctx.yara.rules, file_buffer[0].unsafeAddr(), file_size, SCAN_FLAGS_FAST_MODE, fscanner_on_malware_found_yara, context, YR_SCAN_TIMEOUT)
     discard yr_rules_scan_mem(ctx.yara.rules, cast[ptr uint8](file_buffer[0].unsafeAddr()), file_size, SCAN_FLAGS_FAST_MODE, fscanner_on_malware_found_yara, context, YR_SCAN_TIMEOUT)
   else:
     discard yr_rules_scan_fd(ctx.yara.rules, fd, SCAN_FLAGS_FAST_MODE, fscanner_on_malware_found_yara, context, YR_SCAN_TIMEOUT)
